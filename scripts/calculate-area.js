e4d3ab7b2fe36b042e45d41fdb2e3fc6fc718b06
RabayaSultana <rabayashova811@gmail.com>
2024-07-31 13:59:37 +0600

    Ellipse added

--- scripts/calculate-area.js
+++ scripts/calculate-area.js
@@ -30,6 +30,13 @@ function calculatePentagonArea(){
 
 }
 
+function calculateEllipseArea(){
+    const majorRadius = getInputValueById('ellipse-major-radius');
+    const minorRadius = getInputValueById('ellipse-minor-radius');
+    const area = 3.14 * majorRadius * minorRadius;
+    setInnerTextById('ellipse-area', area);
+}
+
 function getInputValueById(inputFieldId){
     const inputField = document.getElementById(inputFieldId);
     const inputValueText = inputField.value;
